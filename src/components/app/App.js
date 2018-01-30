1d6eac0b3e0e35b53cc23fc1d6ab9d66c3f676aa
G.Provost <grace.g.provost@gmail.com>
2018-01-30 10:09:29 -0800

    add shake motion to page change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import Template from '../Template';
 import html from './app.html';
 import './app.css';
+import '../pages/pages.css';
 import { removeChildren } from '../dom';
 import Header from './Header';
 import Page1 from '../pages/page1/Page1';
